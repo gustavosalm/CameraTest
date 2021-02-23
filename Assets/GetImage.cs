727d62e9e818f57cdd04ceab631593fb753b9a5f
gustavosalm <gustavosalmeida9821@gmail.com>
2021-02-23 20:32:02 -0300

    pegando imagem do pc com path já dado

---
+++ Assets/GetImage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class GetImage : MonoBehaviour
+{    
+    private string filePath = @"C:\Users\GAROTOS\Downloads\photo4940890537658001801.jpg";
+
+    void Start()
+    {
+        GetTheImage();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void GetTheImage(){
+        Texture2D tex = null;
+        byte[] fileData;
+        if (File.Exists(filePath)){
+            fileData = File.ReadAllBytes(filePath);
+            tex = new Texture2D(2, 2);
+            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+        }
+        this.GetComponent<RawImage>().texture = tex;
+    }
+}
