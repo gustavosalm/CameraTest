ddcd1dccd4315bec8323fbebdde702d43bf48b58
gustavosalm <gustavosalmeida9821@gmail.com>
2021-02-24 13:10:37 -0300

    foto cortando meio errado ainda

--- Assets/GetImage.cs
+++ Assets/GetImage.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 
 public class GetImage : MonoBehaviour
 {    
-    private string filePath = @"C:\Users\GAROTOS\Downloads\photo4940890537658001801.jpg";
+    private string filePath = @"C:\gut7xzr9vki51.png";
+    private float newScale = 1;
 
     void Start()
     {
@@ -25,6 +26,15 @@ public class GetImage : MonoBehaviour
             fileData = File.ReadAllBytes(filePath);
             tex = new Texture2D(2, 2);
             tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+            this.GetComponent<RectTransform>().sizeDelta = new Vector2(tex.width, tex.height);
+            float hScale = ((float)Screen.height/(float)tex.height);
+            print(hScale);
+            float wScale = ((float)Screen.width/(float)tex.width);
+            print(wScale);
+            newScale = (hScale < wScale) ? hScale : wScale;
+            this.GetComponent<RectTransform>().localScale = new Vector3(newScale, newScale, 1);
+            print(Screen.height + "  " + tex.height);
+            print(Screen.width + "  " + tex.width);
         }
         this.GetComponent<RawImage>().texture = tex;
     }
--- Assets/TakePicture.cs
+++ Assets/TakePicture.cs
@@ -7,24 +7,47 @@ using UnityEngine.UI;
 public class TakePicture : MonoBehaviour
 {
     private WebCamTexture webCamTexture;
+    private float newScale = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        var width = Camera.main.orthographicSize * 2.0f * Camera.main.aspect;
-        print(Screen.width);
-        print(width);
-        //this.GetComponent<RectTransform>().sizeDelta = new Vector2(width, Camera.main.orthographicSize * 2.0f);
-        
-        //transform.localScale = new Vector3(width, width);
         webCamTexture = new WebCamTexture();
         webCamTexture.Play();
+        GetComponent<RawImage>().texture = webCamTexture;
+        this.GetComponent<RectTransform>().sizeDelta = new Vector2(webCamTexture.width, webCamTexture.height);
+        float hScale = ((float)Screen.height/(float)webCamTexture.height);
+        float wScale = ((float)Screen.width/(float)webCamTexture.width);
+        newScale = /*(hScale < wScale) ? hScale : wScale*/ hScale;
+        this.GetComponent<RectTransform>().localScale = new Vector3(newScale, newScale, 1);
+        print(Screen.height + "  " + webCamTexture.height);
+        print(Screen.width + "  " + webCamTexture.width);
+    }
+    public void StartTakingPicture(){
+        StartCoroutine("TakeAPicture");
+    }
+
+    public IEnumerator TakeAPicture(){
+        yield return new WaitForEndOfFrame();
+
+        Texture2D photo = new Texture2D(webCamTexture.width, webCamTexture.height);
+        photo.SetPixels(webCamTexture.GetPixels());
+        photo.Apply();
+
+        Color[] color = photo.GetPixels((photo.width - 400) / 2, 0, 400, photo.height, 0);
+        Texture2D newTex = new Texture2D(400, webCamTexture.height);
+        newTex.SetPixels(color, 1);
+        newTex.Apply();
+
+        //oImg.GetPixels((oImg.width - cropSize) / 2, (oImg.height - cropSize) / 2, cropSize, cropSize, 0);
+
+        //Encode to a PNG
+        byte[] bytes = newTex.EncodeToPNG();
+        //Write out the PNG. Of course you have to substitute your_path for something sensible
+        File.WriteAllBytes(@"photo.png", bytes);
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(Screen.width + "  " + Screen.height);
-        this.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width, Screen.height);
-        GetComponent<RawImage>().texture = webCamTexture;
     }
 }
