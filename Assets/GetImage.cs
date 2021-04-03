3c047eec84bffc569fe5ee9c281edb0f7fd46c2b
gustavosalm <gustavosalmeida9821@gmail.com>
2021-04-02 21:21:15 -0300

    comprimir imagem

--- Assets/GetImage.cs
+++ Assets/GetImage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
+using System;
 
 public class GetImage : MonoBehaviour
 {    
@@ -11,7 +12,7 @@ public class GetImage : MonoBehaviour
 
     void Start()
     {
-        // GetTheImage();
+
     }
 
     void Update()
@@ -46,29 +47,28 @@ public class GetImage : MonoBehaviour
             fileData = File.ReadAllBytes(filePath);
             tex = new Texture2D(2, 2);
             tex.LoadImage(fileData);
-
             this.transform.forward = Camera.main.transform.forward;
-
             this.GetComponent<RectTransform>().sizeDelta = new Vector2(tex.width, tex.height);
             // this.GetComponent<RectTransform>().sizeDelta = new Vector2(tex.height, tex.height);
-
             float hScale = ((float)Screen.height/(float)tex.height);
             // print(hScale);
             float wScale = ((float)Screen.width/(float)tex.width);
             // print(wScale);
             newScale = (hScale < wScale) ? hScale : wScale;
-
             this.GetComponent<RectTransform>().localScale = new Vector3(newScale, newScale, 1);
             // this.GetComponent<RectTransform>().localScale = new Vector3(hScale, hScale, 1);
-            
             // print(Screen.height + "  " + tex.height);
             // print(Screen.width + "  " + tex.width);
         }
-
+        print(Convert.ToBase64String(tex.EncodeToJPG()).Length);
+        Texture2D scaled = new Texture2D((int)(tex.width*0.7f), (int)(tex.height*0.7f), TextureFormat.ARGB32, true);
+        print(tex.format);
+        Graphics.ConvertTexture(tex, scaled);
+        tex = scaled;
+        print(Convert.ToBase64String(scaled.EncodeToJPG()).Length);
         // Texture2D photo = new Texture2D(tex.height, tex.height);
         // photo.SetPixels(tex.GetPixels((tex.width - tex.height) / 2, 0, tex.height, tex.height, 0));
         // photo.Apply();
-
-        this.GetComponent<RawImage>().texture = tex;
+        this.GetComponent<RawImage>().texture = scaled;
     }
 }
